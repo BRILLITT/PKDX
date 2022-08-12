4ad5d12dbd597e0c3c3fa214735a0377a43e9269
BRILLITT <98791144+BRILLITT@users.noreply.github.com>
2022-08-12 05:26:22 -0500

    Add files via upload

---
+++ src/components/CharacterItem.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const CharacterItem = ({characterUrl}) => {
+
+    const [character, setCharacter] = useState({});
+
+    const navigate = useNavigate();
+
+    useEffect(()=>{
+        axios.get(characterUrl)
+        .then(res=>setCharacter(res.data));
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+
+    console.log(character)
+
+    return (
+        <div onClick={()=> navigate(`/Pokedex/${character.id}`)}>
+            <h3>{character.name}</h3>
+            <img src={character.sprites?.front_default} alt="" />
+
+            <div>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default CharacterItem;
